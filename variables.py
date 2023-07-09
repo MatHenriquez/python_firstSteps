2b0bde4072020ca36ebd38c02d2e83e033c729c6
trechiStrix <matydavila_77@hotmail.com>
2023-07-09 12:09:48 -0300

    variables and conventions

---
+++ variables.py
@@ -0,0 +1,20 @@
+# Python no requiere declarar las variables ni su tipo
+name = "trechi"
+
+x, book = 100, "Harry Potter" # Guardo valores en variables en una sola línea
+
+print(name)
+print(x, book)
+
+# Convenciones (python es case sensitive)
+book_name = "El Principito" # Snake case (más utilizado en python)
+bookName = "El Muro" # Camel case
+BookName = "El Príncipe" # Pascal case
+PI = 3.1416 # Constantes en mayúscula sostenida
+
+# Python es de tipado dinámico
+saludo = "hola"
+saludo = {
+    "hi": "chau"
+}
+
