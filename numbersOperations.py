dbe228af95a4d45d3c8e0db9e2786a638fee5209
trechiStrix <matydavila_77@hotmail.com>
2023-07-09 15:29:45 -0300

    numbers and operations

---
+++ numbersOperations.py
@@ -0,0 +1,10 @@
+print(2 + 3) # Devuelve la suma
+print(3 / 2) # Devuelve el cociente en float
+print(3 // 2) # Devuelve el cociente en int
+print(2**3) # Devuelve la potencia
+print(1 + 1.0) # Devuelve la suma en float
+print(3 % 2) # Devuelve el residuo de la división
+
+# Pido datos al usuario
+age = input('Ingrese su edad: ') # Toma el valor ingresado como string
+print(float(age)) # Lo convierto en float 
