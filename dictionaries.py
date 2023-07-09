b2004f0f44e26d61bde17c95db32d9d602abfa5b
trechiStrix <matydavila_77@hotmail.com>
2023-07-09 18:18:13 -0300

    dictionaries methods

---
+++ dictionaries.py
@@ -0,0 +1,22 @@
+book = {
+    "title": "Harry Potter and the Sorcerer Stone",
+    "author": "J. K. Rowling",
+    "price": "19.99"
+}
+
+character = {
+    "first_name": "Harry",
+    "last_name": "Potter",
+    "ability": "magic"
+}
+
+print(book.keys()) # Imprime sólo las claves en una lista
+
+print(book.get('title')) # Imprime sólo el título 
+
+book.clear() # Deja el diccionario vacío
+
+del character # Elimina el diccionario
+
+
+
